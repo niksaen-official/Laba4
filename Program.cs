f024a951f8d1e372ae79af830a56ab348c762452
Nikita <niksaen.games.official@gmail.com>
2023-02-08 18:10:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба4 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<int> sms = new List<int>();
+            for(int i = 0; i < 10; i++) {
+                sms.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine("Итоги:");
+            for(int i = 1; i < 17; i++) {
+                int filter = i;
+                int count = 0;
+                for(int k = 0;k < sms.Count; k++) {
+                    if (sms[k] == filter) {
+                        count++;
+                    }
+                }
+                Console.WriteLine(i + " " + count);
+            }
+            Console.ReadKey();
+        }
+    }
+}
